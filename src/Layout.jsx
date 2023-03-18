eed233c64d84f4ab6106e78a71953f23f931c643
surya <balasurya811@gmail.com>
2023-03-18 17:55:25 +0530

    Fixes forgotten quotaion and indentation issues.

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,15 +1,15 @@
 import LeftNavBar from "./components/LeftNavBar";
 import React from "react";
-import {Outlet} from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Explore from "./components/Explore";
 
 export default function Layout() {
 
   return (
-      <div className="App">
-        <LeftNavBar className="navbar"/>
-        <Outlet/>
-        <Explore/>
-      </div>
-  )
+    <div className="App">
+      <LeftNavBar className="navbar" />
+      <Outlet />
+      <Explore />
+    </div>
+  );
 }
